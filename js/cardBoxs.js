cfc194a23eff73db0ae0f23b89b426fa689f6e0f
shuvo903 <saifulislamshuvoa123@gmail.com>
2025-03-03 08:39:27 +0600

    added click button show alart in this page

---
+++ js/cardBoxs.js
@@ -0,0 +1,175 @@
+// ---------------------Card - 1 -----------------------------
+
+document.getElementById('cardBtn-1').addEventListener('click',function(){
+    alert('Board Update Successfully')
+
+
+
+    // Minus-the Task Assigned points in a click =>
+
+    const taskAssigned = document.getElementById('Minus-point').innerText;
+    const convartedTaskAssigned = parseFloat(taskAssigned);
+    const minus = convartedTaskAssigned - 1;
+    document.getElementById('Minus-point').innerText = minus;
+    
+    // Sum-the Task Assigned points in a click =>
+
+    const navBarPoint = document.getElementById('navbar-point').innerText;
+    const convartedNavBarPoint = parseFloat(navBarPoint);
+    const sum = convartedNavBarPoint + 1;
+    document.getElementById('navbar-point').innerText = sum;
+})
+
+function btn1Disabled(){
+    document.getElementById('cardBtn-1').disabled = true;
+}
+// ---------------------Card - 2 -----------------------------
+
+document.getElementById('cardBtn-2').addEventListener('click',function(){
+    alert('Board Update Successfully')
+
+
+
+    // Minus-the Task Assigned points in a click =>
+
+    const taskAssigned = document.getElementById('Minus-point').innerText;
+    const convartedTaskAssigned = parseFloat(taskAssigned);
+    const minus = convartedTaskAssigned - 1;
+    document.getElementById('Minus-point').innerText = minus;
+    
+    // Sum-the Task Assigned points in a click =>
+
+    const navBarPoint = document.getElementById('navbar-point').innerText;
+    const convartedNavBarPoint = parseFloat(navBarPoint);
+    const sum = convartedNavBarPoint + 1;
+    document.getElementById('navbar-point').innerText = sum;
+
+
+    
+    
+})
+
+function btn2Disabled(){
+    document.getElementById('cardBtn-2').disabled = true;
+}
+// ---------------------Card - 3 -----------------------------
+
+document.getElementById('cardBtn-3').addEventListener('click',function(){
+    alert('Board Update Successfully')
+
+
+
+    // Minus-the Task Assigned points in a click =>
+
+    const taskAssigned = document.getElementById('Minus-point').innerText;
+    const convartedTaskAssigned = parseFloat(taskAssigned);
+    const minus = convartedTaskAssigned - 1;
+    document.getElementById('Minus-point').innerText = minus;
+    
+    // Sum-the Task Assigned points in a click =>
+
+    const navBarPoint = document.getElementById('navbar-point').innerText;
+    const convartedNavBarPoint = parseFloat(navBarPoint);
+    const sum = convartedNavBarPoint + 1;
+    document.getElementById('navbar-point').innerText = sum;
+
+
+    
+    
+})
+
+function btn3Disabled(){
+    document.getElementById('cardBtn-3').disabled = true;
+}
+// ---------------------Card - 4 -----------------------------
+
+document.getElementById('cardBtn-4').addEventListener('click',function(){
+    alert('Board Update Successfully')
+
+
+
+    // Minus-the Task Assigned points in a click =>
+
+    const taskAssigned = document.getElementById('Minus-point').innerText;
+    const convartedTaskAssigned = parseFloat(taskAssigned);
+    const minus = convartedTaskAssigned - 1;
+    document.getElementById('Minus-point').innerText = minus;
+    
+    // Sum-the Task Assigned points in a click =>
+
+    const navBarPoint = document.getElementById('navbar-point').innerText;
+    const convartedNavBarPoint = parseFloat(navBarPoint);
+    const sum = convartedNavBarPoint + 1;
+    document.getElementById('navbar-point').innerText = sum;
+
+
+    
+    
+})
+
+function btn4Disabled(){
+    document.getElementById('cardBtn-4').disabled = true;
+}
+// ---------------------Card - 5 -----------------------------
+
+document.getElementById('cardBtn-5').addEventListener('click',function(){
+    alert('Board Update Successfully')
+
+
+
+    // Minus-the Task Assigned points in a click =>
+
+    const taskAssigned = document.getElementById('Minus-point').innerText;
+    const convartedTaskAssigned = parseFloat(taskAssigned);
+    const minus = convartedTaskAssigned - 1;
+    document.getElementById('Minus-point').innerText = minus;
+    
+    // Sum-the Task Assigned points in a click =>
+
+    const navBarPoint = document.getElementById('navbar-point').innerText;
+    const convartedNavBarPoint = parseFloat(navBarPoint);
+    const sum = convartedNavBarPoint + 1;
+    document.getElementById('navbar-point').innerText = sum;
+
+
+    
+    
+})
+
+function btn5Disabled(){
+    document.getElementById('cardBtn-5').disabled = true;
+}
+// ---------------------Card - 6 -----------------------------
+
+document.getElementById('cardBtn-6').addEventListener('click',function(){
+    alert('Board Update Successfully')
+
+
+
+    // Minus-the Task Assigned points in a click =>
+
+    const taskAssigned = document.getElementById('Minus-point').innerText;
+    const convartedTaskAssigned = parseFloat(taskAssigned);
+    const minus = convartedTaskAssigned - 1;
+    document.getElementById('Minus-point').innerText = minus;
+    
+    // Sum-the Task Assigned points in a click =>
+
+    const navBarPoint = document.getElementById('navbar-point').innerText;
+    const convartedNavBarPoint = parseFloat(navBarPoint);
+    const sum = convartedNavBarPoint + 1;
+    document.getElementById('navbar-point').innerText = sum;
+
+
+    
+    
+})
+
+function btn6Disabled(){
+    document.getElementById('cardBtn-6').disabled = true;
+}
+
+
+//  const button2 = document.getElementById('cardBtn-2').addEventListener('click',function(){
+//     button2.disabled = true;
+// })
