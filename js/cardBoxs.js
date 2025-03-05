5559a0c3ed0abf3422f301fe781d393f8204010f
shuvo <saifulislamshuvoa123@gmail.com>
2025-03-05 16:33:22 +0600

    added Activity Log section full work and clear History btn working

--- js/cardBoxs.js
+++ js/cardBoxs.js
@@ -1,6 +1,6 @@
 // ---------------------Card - 1 -----------------------------
 
-document.getElementById('cardBtn-1').addEventListener('click',function(){
+document.getElementById('cardBtn-1').addEventListener('click', function () {
     alert('Board Update Successfully')
 
 
@@ -10,30 +10,47 @@ document.getElementById('cardBtn-1').addEventListener('click',function(){
     const taskAssigned = document.getElementById('Minus-point').innerText;
     const convartedTaskAssigned = parseFloat(taskAssigned);
     const minus = convartedTaskAssigned - 1;
-    if(minus === 0){
+    if (minus === 0) {
         alert('Congrats!!! You have Completed all the current task')
     }
-    else{
-        
+    else {
+
     }
     document.getElementById('Minus-point').innerText = minus;
 
-
- 
     // Sum-the Task Assigned points in a click =>
 
     const navBarPoint = document.getElementById('navbar-point').innerText;
     const convartedNavBarPoint = parseFloat(navBarPoint);
     const sum = convartedNavBarPoint + 1;
     document.getElementById('navbar-point').innerText = sum;
+
+    let now = new Date();
+    let currentTime = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+
+    const theHistory = document.getElementById('Activity-History')
+
+    const cardTitle1 = document.getElementById('card-1-title').innerText;
+
+
+    const div = document.createElement('div');
+
+    div.innerHTML = `
+<h1 id="popHistory1" class="h-auto w-auto bg-[#F4F7FF] mt-7 ml-7 mr-4 rounded-lg px-3 py-3" > You have Complete The Task ${cardTitle1} at ${currentTime} </h1>
+`
+    theHistory.appendChild(div);
+
+    document.getElementById('clearHistoryBtn').addEventListener('click', function () {
+        document.getElementById('popHistory1').style.display = 'none';
+    })
 })
 
-function btn1Disabled(){
+function btn1Disabled() {
     document.getElementById('cardBtn-1').disabled = true;
 }
 // ---------------------Card - 2 -----------------------------
 
-document.getElementById('cardBtn-2').addEventListener('click',function(){
+document.getElementById('cardBtn-2').addEventListener('click', function () {
     alert('Board Update Successfully')
 
 
@@ -43,14 +60,14 @@ document.getElementById('cardBtn-2').addEventListener('click',function(){
     const taskAssigned = document.getElementById('Minus-point').innerText;
     const convartedTaskAssigned = parseFloat(taskAssigned);
     const minus = convartedTaskAssigned - 1;
-    if(minus === 0){
+    if (minus === 0) {
         alert('Congrats!!! You have Completed all the current task')
     }
-    else{
-        
+    else {
+
     }
     document.getElementById('Minus-point').innerText = minus;
-    
+
     // Sum-the Task Assigned points in a click =>
 
     const navBarPoint = document.getElementById('navbar-point').innerText;
@@ -58,17 +75,32 @@ document.getElementById('cardBtn-2').addEventListener('click',function(){
     const sum = convartedNavBarPoint + 1;
     document.getElementById('navbar-point').innerText = sum;
 
+    let now = new Date();
+    let currentTime = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
 
-    
-    
+    const theHistory = document.getElementById('Activity-History')
+
+    const cardTitle1 = document.getElementById('card-2-title').innerText;
+
+
+    const div = document.createElement('div');
+
+    div.innerHTML = `
+<h1 id="popHistory2" class="h-auto w-auto bg-[#F4F7FF] mt-7 ml-7 mr-4 rounded-lg px-3 py-3" > You have Complete The Task ${cardTitle1} at ${currentTime} </h1>
+`
+    theHistory.appendChild(div);
+
+    document.getElementById('clearHistoryBtn').addEventListener('click', function () {
+        document.getElementById('popHistory2').style.display = 'none';
+    })
 })
 
-function btn2Disabled(){
+function btn2Disabled() {
     document.getElementById('cardBtn-2').disabled = true;
 }
 // ---------------------Card - 3 -----------------------------
 
-document.getElementById('cardBtn-3').addEventListener('click',function(){
+document.getElementById('cardBtn-3').addEventListener('click', function () {
     alert('Board Update Successfully')
 
 
@@ -78,14 +110,14 @@ document.getElementById('cardBtn-3').addEventListener('click',function(){
     const taskAssigned = document.getElementById('Minus-point').innerText;
     const convartedTaskAssigned = parseFloat(taskAssigned);
     const minus = convartedTaskAssigned - 1;
-    if(minus === 0){
+    if (minus === 0) {
         alert('Congrats!!! You have Completed all the current task')
     }
-    else{
-        
+    else {
+
     }
     document.getElementById('Minus-point').innerText = minus;
-    
+
     // Sum-the Task Assigned points in a click =>
 
     const navBarPoint = document.getElementById('navbar-point').innerText;
@@ -93,17 +125,32 @@ document.getElementById('cardBtn-3').addEventListener('click',function(){
     const sum = convartedNavBarPoint + 1;
     document.getElementById('navbar-point').innerText = sum;
 
+    let now = new Date();
+    let currentTime = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
 
-    
-    
+    const theHistory = document.getElementById('Activity-History')
+
+    const cardTitle1 = document.getElementById('card-3-title').innerText;
+
+
+    const div = document.createElement('div');
+
+    div.innerHTML = `
+<h1 id="popHistory3" class="h-auto w-auto bg-[#F4F7FF] mt-7 ml-7 mr-4 rounded-lg px-3 py-3" > You have Complete The Task ${cardTitle1} at ${currentTime} </h1>
+`
+    theHistory.appendChild(div);
+
+    document.getElementById('clearHistoryBtn').addEventListener('click', function () {
+        document.getElementById('popHistory3').style.display = 'none';
+    })
 })
 
-function btn3Disabled(){
+function btn3Disabled() {
     document.getElementById('cardBtn-3').disabled = true;
 }
 // ---------------------Card - 4 -----------------------------
 
-document.getElementById('cardBtn-4').addEventListener('click',function(){
+document.getElementById('cardBtn-4').addEventListener('click', function () {
     alert('Board Update Successfully')
 
 
@@ -113,14 +160,14 @@ document.getElementById('cardBtn-4').addEventListener('click',function(){
     const taskAssigned = document.getElementById('Minus-point').innerText;
     const convartedTaskAssigned = parseFloat(taskAssigned);
     const minus = convartedTaskAssigned - 1;
-    if(minus === 0){
+    if (minus === 0) {
         alert('Congrats!!! You have Completed all the current task')
     }
-    else{
-        
+    else {
+
     }
     document.getElementById('Minus-point').innerText = minus;
-    
+
     // Sum-the Task Assigned points in a click =>
 
     const navBarPoint = document.getElementById('navbar-point').innerText;
@@ -128,17 +175,32 @@ document.getElementById('cardBtn-4').addEventListener('click',function(){
     const sum = convartedNavBarPoint + 1;
     document.getElementById('navbar-point').innerText = sum;
 
+    let now = new Date();
+    let currentTime = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
 
-    
-    
+    const theHistory = document.getElementById('Activity-History')
+
+    const cardTitle1 = document.getElementById('card-4-title').innerText;
+
+
+    const div = document.createElement('div');
+
+    div.innerHTML = `
+<h1 id="popHistory4" class="h-auto w-auto bg-[#F4F7FF] mt-7 ml-7 mr-4 rounded-lg px-3 py-3" > You have Complete The Task ${cardTitle1} at ${currentTime} </h1>
+`
+    theHistory.appendChild(div);
+
+    document.getElementById('clearHistoryBtn').addEventListener('click', function () {
+        document.getElementById('popHistory4').style.display = 'none';
+    })
 })
 
-function btn4Disabled(){
+function btn4Disabled() {
     document.getElementById('cardBtn-4').disabled = true;
 }
 // ---------------------Card - 5 -----------------------------
 
-document.getElementById('cardBtn-5').addEventListener('click',function(){
+document.getElementById('cardBtn-5').addEventListener('click', function () {
     alert('Board Update Successfully')
 
 
@@ -148,14 +210,14 @@ document.getElementById('cardBtn-5').addEventListener('click',function(){
     const taskAssigned = document.getElementById('Minus-point').innerText;
     const convartedTaskAssigned = parseFloat(taskAssigned);
     const minus = convartedTaskAssigned - 1;
-    if(minus === 0){
+    if (minus === 0) {
         alert('Congrats!!! You have Completed all the current task')
     }
-    else{
-        
+    else {
+
     }
     document.getElementById('Minus-point').innerText = minus;
-    
+
     // Sum-the Task Assigned points in a click =>
 
     const navBarPoint = document.getElementById('navbar-point').innerText;
@@ -163,17 +225,33 @@ document.getElementById('cardBtn-5').addEventListener('click',function(){
     const sum = convartedNavBarPoint + 1;
     document.getElementById('navbar-point').innerText = sum;
 
+    let now = new Date();
+    let currentTime = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
 
-    
-    
+    const theHistory = document.getElementById('Activity-History')
+
+    const cardTitle1 = document.getElementById('card-5-title').innerText;
+
+
+    const div = document.createElement('div');
+
+    div.innerHTML = `
+<h1 id="popHistory5" class="h-auto w-auto bg-[#F4F7FF] mt-7 ml-7 mr-4 rounded-lg px-3 py-3" > You have Complete The Task ${cardTitle1} at ${currentTime} </h1>
+`
+    theHistory.appendChild(div);
+
+    document.getElementById('clearHistoryBtn').addEventListener('click', function () {
+        document.getElementById('popHistory5').style.display = 'none';
+    })
+
 })
 
-function btn5Disabled(){
+function btn5Disabled() {
     document.getElementById('cardBtn-5').disabled = true;
 }
 // ---------------------Card - 6 -----------------------------
 
-document.getElementById('cardBtn-6').addEventListener('click',function(){
+document.getElementById('cardBtn-6').addEventListener('click', function () {
     alert('Board Update Successfully')
 
 
@@ -183,18 +261,18 @@ document.getElementById('cardBtn-6').addEventListener('click',function(){
     const taskAssigned = document.getElementById('Minus-point').innerText;
     const convartedTaskAssigned = parseFloat(taskAssigned);
     const minus = convartedTaskAssigned - 1;
-    
 
 
-    if(minus === 0){
+
+    if (minus === 0) {
         alert('Congrats!!! You have Completed all the current task')
     }
-    else{
-        
+    else {
+
     }
- 
+
     document.getElementById('Minus-point').innerText = minus;
-    
+
     // Sum-the Task Assigned points in a click =>
 
     const navBarPoint = document.getElementById('navbar-point').innerText;
@@ -202,12 +280,31 @@ document.getElementById('cardBtn-6').addEventListener('click',function(){
     const sum = convartedNavBarPoint + 1;
     document.getElementById('navbar-point').innerText = sum;
 
+    let now = new Date();
+    let currentTime = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
 
-    
+    const theHistory = document.getElementById('Activity-History')
+
+    const cardTitle1 = document.getElementById('card-6-title').innerText;
+
+
+    const div = document.createElement('div');
+
+    div.innerHTML = `
+<h1 id="popHistory6" class="h-auto w-auto bg-[#F4F7FF] mt-7 ml-7 mr-4 rounded-lg px-3 py-3" > You have Complete The Task ${cardTitle1} at ${currentTime} </h1>
+`
+    theHistory.appendChild(div);
+
+    document.getElementById('clearHistoryBtn').addEventListener('click', function () {
+        document.getElementById('popHistory6').style.display = 'none';
+    })
+
 })
 
-function btn6Disabled(){
+function btn6Disabled() {
     document.getElementById('cardBtn-6').disabled = true;
 }
+
+
 
 
--- js/index.js
+++ js/index.js
@@ -1,11 +1,29 @@
 let btn = document.getElementById("randomColor");
 let bgColor = document.getElementById("chengingColor");
 function getRandomColor() {
-  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+    return "#" + Math.floor(Math.random() * 16777215).toString(16);
 }
 btn.addEventListener("click", (event) => {
-  
+
     document.body.style.backgroundColor = getRandomColor();
 
 });
+const date = new Date();
+const dayName = date.toLocaleString('default', { weekday: 'short' });
+const day = date.getDate();
+const monthName = date.toLocaleString('default', { month: 'short' });
+const year = date.getFullYear();
 
+console.log(`${day} ${monthName} ${year}`);
+
+const realDate = document.getElementById('dayYearName');
+
+
+const div = document.createElement('div');
+
+div.innerHTML = `
+<h1 class="text-2xl">${dayName},</h1>
+
+<h2 class="text-xl font-bold">${monthName} ${day} ${year}</h2>
+`
+realDate.appendChild(div);
